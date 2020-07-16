ee4478122df4210d098b2e090cbe7d91cde9d643
nmcanall <nmcanall@citadel.edu>
2020-07-15 21:31:27 -0700

    prompt questions for one project

--- app.js
+++ app.js
@@ -10,12 +10,68 @@
 // })
 
 const inquirer = require("inquirer");
-inquirer
-    .prompt([{
-        type: "input",
-        name: "name",
-        message: "What is your name?"
-    }])
-    .then(answers => {
-        return console.log(answers)
-    });
+
+// Prompt user for profile information
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your GitHub username?"
+        },
+        {
+            type: "input",
+            name: "about",
+            message: "Provide some information about yourself:"
+        }
+    ]);
+}; 
+
+// Prompt user for project information
+const promptProject = () => {
+    console.log(`
+=================
+Add a New Project
+=================
+`);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: "name",
+            message: "What is the name of your project?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Provide a description of the project (Required)"
+        },
+        {
+            type: "checkbox",
+            name: "languages",
+            message: "What did you use this project with? (Check all that apply)",
+            choices: ["JavaScript", "HTML", "CSS", "ES6", "jQuery", "Bootstrap", "Node"]
+        },
+        {
+            type: "confirm",
+            name: "feature",
+            message: "Would you like this to feature this project?",
+            default: false
+        },
+        {
+            type: "confirm",
+            name: "confirmAddProject",
+            message: "Would you like to enter another project?",
+            default: false
+        }
+    ]);
+};
+
+promptUser()
+    .then(answers => console.log(answers))
+    .then(promptProject)
+    .then(projectAnswers => console.log(projectAnswers));
