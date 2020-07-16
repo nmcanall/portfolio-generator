1b1578200b3d773f6786513c734193a36f8e2526
nmcanall <nmcanall@citadel.edu>
2020-07-15 21:03:30 -0700

    import inquirer and inquirer example

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const fs = require("fs");
-const generatePage = require("./src/page-template.js");
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+// const fs = require("fs");
+// const generatePage = require("./src/page-template.js");
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const [name, github] = profileDataArgs;
 
-fs.writeFile("index.html", generatePage(name, github), err => {
-    if(err) throw err;
+// fs.writeFile("index.html", generatePage(name, github), err => {
+//     if(err) throw err;
 
-    console.log("Portfolio complete!");
-})
+//     console.log("Portfolio complete!");
+// })
+
+const inquirer = require("inquirer");
+inquirer
+    .prompt([{
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+    }])
+    .then(answers => {
+        return console.log(answers)
+    });
