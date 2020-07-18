295a6832cbd538f7fda2b37ccdd0972bd5e3783c
nmcanall <nmcanall@citadel.edu>
2020-07-18 15:11:13 -0700

    asynchronous build

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
-const generatePage = require("./src/page-template.js");
 const inquirer = require("inquirer");
+const generatePage = require("./src/page-template.js");
+const {writeFile, copyFile} = require("./utils/generate-site.js");
 
 // Prompt user for profile information
 const promptUser = () => {
@@ -132,14 +133,20 @@ Add a New Project
 };
 
 promptUser()
-    // .then(answers => console.log(answers))
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-
-        fs.writeFile("./index.html", pageHTML, err => {
-            if(err) throw new Error(err);
-            
-            console.log("Page created!");
-        })
+        return generatePage(portfolioData)
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML)
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse)
+    })
+    .catch(err => {
+        console.log(err);
     });
